5ad104f666bc103157739a52c027b3f48e186105
zhoami21 <minghao.zhao16@gmail.com>
2024-10-25 08:09:00 -0400

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Pangolin } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const pangolin = Pangolin({
   weight: '400',
@@ -9,8 +10,12 @@ const pangolin = Pangolin({
 })
 
 export const metadata = {
-  title: 'Invoicing App',
-  description: 'Create and manage invoices easily',
+  title: 'Track Invoicing',
+  description: 'Create and manage invoices with ease',
+  icons: [
+    { rel: 'icon', url: '/favicon.ico' },
+    { rel: 'apple-touch-icon', url: '/apple-touch-icon.png' },
+  ],
 }
 
 export default function RootLayout({
@@ -20,9 +25,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${pangolin.variable}`}>
-      <body>
-        {children}
-      </body>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <body>{children}</body>
     </html>
   )
 }
