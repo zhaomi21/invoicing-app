1322c3f5b2f1f276e5f5315f784d85905da82f56
zhoami21 <minghao.zhao16@gmail.com>
2024-10-24 21:58:45 -0400

    fix font cofig

---
+++ app/fonts.ts
@@ -0,0 +1,21 @@
+import localFont from 'next/font/local'
+
+export const geistSans = localFont({
+  src: [
+    {
+      path: '../public/fonts/Geist-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Geist-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Geist-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+})
